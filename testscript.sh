825e8260ca8c1226babc9e77d50b9e80ac8ba1e3
chandraprakashv <45370633+chandraprakashv@users.noreply.github.com>
2019-07-13 14:54:25 +0530

    2 time update

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "added for auto scm poll"
+echo "added for auto scm poll- 2 time"
 echo "this was run automagically"
