b02b9f70844798b49fbf58551caa45ec0f86f7b4
chandraprakash <chandraprakash.v@gmail.com>
2019-07-13 14:58:22 +0530

    third time

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "added for auto scm poll"
 echo "added for auto scm poll- 2 time"
+echo "third time is a charm"
 echo "this was run automagically"
